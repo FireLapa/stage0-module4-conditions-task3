423c851e0b1e4cf6d3f44c40546f8a5c80569921
Lapa <lapabig@gmail.com>
2023-05-16 15:59:30 +0600

    Updated

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -1,6 +1,20 @@
 package school.mjc.stage0.conditions.task3;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        String simvol = Character.toString(character);
+
+        Matcher match1 = Pattern.compile("[aioeuyAIOEUY]").matcher(simvol);
+        Matcher match2 = Pattern.compile("[a-zA-Z]").matcher(simvol);
+        if (match1.find()) {
+            System.out.println("Vowel");
+        } else if (match2.find()) {
+            System.out.println("Consonant");
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
